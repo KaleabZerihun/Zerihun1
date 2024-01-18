9e1bf4d8d33a89d581e66a72b75fb23143002819
KaleabZerihun <Kaleab.Zerihun@jacks.sdstate.edu>
2024-01-18 12:57:44 -0600

    inital commit

--- Zerihun1/Program.cs
+++ Zerihun1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Helo, Wordl!");
+Console.WriteLine("Kaleab Zerihun");
+Console.WriteLine("CSC 346");
+Console.WriteLine("Assignmnet 1");
+Console.WriteLine("01-24-24");
